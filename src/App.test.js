ad15824cbf54db16634264235ca796f0fc3c6d6a
Angela Collings <angelacollings@developersinstitute.co.nz>
2022-02-13 16:24:35 +1300

    add describe block

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders photo', () => {
+describe("Given that the page has a photo", () => {
+  test('Then it renders the photo or alt text', () => {
   render(<App />);
   const imageElement = screen.getByRole('img');
   expect(imageElement).toBeInTheDocument();
   expect(imageElement).toHaveAttribute('src', 'photo.png');
   expect(imageElement).toHaveAttribute('alt', 'me');
 });
+})
 
