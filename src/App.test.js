1bed7d374c0ec5ed3fff79dd92982bc597cdd5c7
Angela Collings <angelacollings@developersinstitute.co.nz>
2022-02-13 16:25:14 +1300

    write test for info component

--- src/App.test.js
+++ src/App.test.js
@@ -3,11 +3,20 @@ import App from './App';
 
 describe("Given that the page has a photo", () => {
   test('Then it renders the photo or alt text', () => {
+    render(<App />);
+    const imageElement = screen.getByRole('img');
+    expect(imageElement).toBeInTheDocument();
+    expect(imageElement).toHaveAttribute('src', 'photo.png');
+    expect(imageElement).toHaveAttribute('alt', 'me');
+  });
+})
+describe("Given that the page has an info component", () => {
+  test('Then name and title exist', () => {
   render(<App />);
-  const imageElement = screen.getByRole('img');
-  expect(imageElement).toBeInTheDocument();
-  expect(imageElement).toHaveAttribute('src', 'photo.png');
-  expect(imageElement).toHaveAttribute('alt', 'me');
+  const nameElement = screen.getByText(/Angela Collings/i);
+  expect(nameElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Software Developer/i);
+  expect(titleElement).toBeInTheDocument();
 });
 })
 
