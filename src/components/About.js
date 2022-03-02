4e8632c687b95075f7fef9ecb58ca4f378134c9c
Angela Collings <angelacollings@developersinstitute.co.nz>
2022-03-02 18:18:57 +1300

    implement about component

---
+++ src/components/About.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="About">
+        <h3>About</h3>
+        <p>
+            I am a budding software developer 
+            with a passion for problem solving and clean, concise code.
+            I enjoy learning new things, 
+            and I try to keep up with latest technologies 
+            and industry best practices.
+        </p>
+        <h3>Interests</h3>
+        <p>
+            Coffee lover. 
+            Self appointed master chef. 
+            Funky sock collector. 
+            Dinosaur aficionado. 
+            Music buff. 
+            Gamer.
+        </p>
+        </div>
+    )
+};
+
+export default About;
