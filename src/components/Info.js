12f17794af6f78814258d0246b699ba6cd27cac2
Angela Collings <angelacollings@developersinstitute.co.nz>
2022-03-02 18:19:54 +1300

    indentation, and add className

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,12 +3,12 @@ import photo from '../photo.png';
 
 const Info = () => {
     return (
-        <header>
-        <img src={photo} className="photo" alt="me" />
-        <h1>Angela Collings</h1>
-        <h2>Software Developer</h2>
-        <button onClick={() => window.location = 'mailto:angecollings@gmail.com'}><i class="fa fa-envelope"></i> Email</button>
-        </header>
+        <div className="Info">
+            <img src={photo} className="photo" alt="me" />
+            <h1>Angela Collings</h1>
+            <h2>Software Developer</h2>
+            <button onClick={() => window.location = 'mailto:angecollings@gmail.com'}><i class="fa fa-envelope"></i> Email</button>
+        </div>
     )
 };
 
