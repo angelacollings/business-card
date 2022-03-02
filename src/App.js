9528d91be070d7162b8b780fd918a07f1331e075
Angela Collings <angelacollings@developersinstitute.co.nz>
2022-03-02 19:05:32 +1300

    un-comment about and socials components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Info from "./components/Info";
+import About from "./components/About"
+import Socials from "./components/Socials"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <Info />
-        {/* <About /> */}
-        {/* <Socials /> */}
+        <About />
+        <Socials />
     </div>
   );
 }
