676ecd723d1ad24912d4c0cb07de4bb5905da62c
Angela Collings <angelacollings@developersinstitute.co.nz>
2022-03-02 17:35:27 +1300

    remove un-used import

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import userEvent from "@testing-library/user-event";
 import App from './App';
 
 describe("Given that the page has a photo", () => {
