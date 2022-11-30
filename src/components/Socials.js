d52049b33016d71a05593026205baa937e79dc1c
Angela Collings <angelacollings@developersinstitute.co.nz>
2022-12-01 11:55:15 +1300

    remove redundent classNames

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -5,19 +5,16 @@ const Socials = () => {
   return (
     <div className="Socials">
       <button
-        className="Button"
         onClick={() =>
           window.open("https://www.linkedin.com/in/angelacollings/", "_blank")
         }
         class="fa fa-linkedin"
       ></button>
       <button
-        className="Button"
         onClick={() => (window.location = "mailto:angecollings@gmail.com")}
         class="fa fa-envelope"
       ></button>
       <button
-        className="Button"
         onClick={() =>
           window.open("https://github.com/angelacollings", "_blank")
         }
