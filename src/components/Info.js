376c56cc37072f4915169fab29c1253d23084279
Angela Collings <angelacollings@developersinstitute.co.nz>
2022-11-30 19:43:01 +1300

    add photo

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,10 +1,11 @@
 import React from "react";
-import photo from '../photo.png';
+import photo from '../photo.jpeg';
+import '../App.css';
 
 const Info = () => {
     return (
         <div className="Info">
-            <img src={photo} className="photo" alt="me" />
+            <img src={photo} className="photo" alt="me"/>
             <h1>Angela Collings</h1>
             <h2>Software Developer</h2>
             <button onClick={() => window.location = 'mailto:angecollings@gmail.com'} class="fa fa-envelope"> Email</button>
